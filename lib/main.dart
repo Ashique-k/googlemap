81692f48b98d05f1a47c2d4e1e4912773c6d21d2
Ashique-K <ashique>
2024-02-08 00:16:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_pjt/gps_pjt.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Map_page(),
+    );
+  }
+}
